2ad8526df721eb3a674c7826723f2fca13824821
array19 <arai19.00.12@gmail.com>
2022-06-01 19:57:02 +0700

    add show commment in uploadans.php

--- index.php
+++ index.php
@@ -34,7 +34,6 @@ if (isset($_GET['search'])) {
         </div>
     </div>
 </section>
-
 <?php
 include 'includes/footer.php'
 ?>
--- uploadans.php
+++ uploadans.php
@@ -7,8 +7,9 @@ if(!isset($_SESSION["username"])) {
     header("Location: login and register/login_page.php");
 }
 $id_tanya = $_GET["pertanyaan"];
-$query = pg_query("SELECT * FROM pertanyaan WHERE id_pertanyaan='$id_tanya'");
-$content = pg_fetch_array($query);
+$tanya = pg_query("SELECT * FROM pertanyaan WHERE id_pertanyaan='$id_tanya'");
+$content = pg_fetch_array($tanya);
+$komentar = pg_query("SELECT * FROM j_k WHERE id_pertanyaan='$id_tanya'");
 ?>
 <body>
     <br><br><br><br><br><br>
@@ -22,4 +23,23 @@ $content = pg_fetch_array($query);
         <br>
         <button type="submit" name="submit">Kirim Komentar</button>
     </form>
+
+    <section id="Question" class="section-bg">
+        <div class="row gx-5" style="margin: auto;">
+            <div class="col">
+                <h1>Komentar</h1>            
+                <?php while($result = pg_fetch_array($komentar)) :?>
+                <div class="p-5">
+                    <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="100">
+                        <div class="member-info">
+                        <h4><?php //$result['username']; ?></h4>
+                        <span><?= $result['komentar']; ?></span>
+                        <!-- <img src="" alt=""> -->
+                        </div>
+                    </div>
+                </div>
+            <?php endwhile; ?>
+            </div>
+        </div>
+    </section>
 </body>
